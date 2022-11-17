43399f58300f2a08a76355562d8f9747b9b99b5b
Дмитрий Смирнов <painven@gmail.com>
2022-11-17 22:43:05 +0300

    Добавлена текстура и основные секции главной формы

---
+++ VodovozTestApp/Infrastructure/Commands/Base/LambdaCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace VodovozTestApp.Infrastructure.Commands;
+
+public class LambdaCommand : CommandBase
+{
+    private readonly Action<object> action;
+    private readonly Func<object, bool> canExecute;
+    private string? login;
+    private Func<object, bool> p;
+
+    public LambdaCommand(Action<object> action, Func<object, bool> canExecute = null)
+    {
+        this.action = action ?? throw new ArgumentNullException(nameof(action));
+        this.canExecute = canExecute;
+    }
+
+    public LambdaCommand(string? login, Func<object, bool> p)
+    {
+        this.login = login;
+        this.p = p;
+    }
+
+    public override bool CanExecute(object parameter)
+    {
+        return canExecute?.Invoke(parameter) ?? true;
+    }
+
+    public override void Execute(object parameter)
+    {
+        action(parameter);
+    }
+}
--- VodovozTestApp/ViewModels/MainWindowViewModel.cs
+++ VodovozTestApp/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace VodovozTestApp.ViewModels;
+﻿using System;
+using System.Windows.Input;
+using VodovozTestApp.Infrastructure.Commands;
+
+namespace VodovozTestApp.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    string title = "Тестовое приложение. Веселый Водовоз";
+    public string Title
+    {
+        get => title;
+        set => Set(ref title, value);
+    }
 
+    public ICommand LoadedCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        LoadedCommand = new LambdaCommand(e => Title += $" загружено в {DateTime.Now}");
+    }
 }
